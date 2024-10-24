86fb8648d642aa4b63e70cd7827fc4b8c19fb453
taew0o <61934715+taew0o@users.noreply.github.com>
2024-10-24 15:11:55 +0900

    [Silver III] Title: 등수 매기기, Time: 780 ms, Memory: 57668 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] grade = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            grade[i] = Integer.parseInt(br.readLine());
+
+        }
+        Arrays.sort(grade);
+        long result = 0;
+        for(int i = 0 ; i < N ; i++){
+            result += Math.abs(grade[i] - (i + 1));
+        }
+        System.out.println(result);
+    }
+}
