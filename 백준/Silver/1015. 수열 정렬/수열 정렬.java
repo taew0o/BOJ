c09a183a32b5e58bf476cac1761fac364bcb7b7c
taew0o <61934715+taew0o@users.noreply.github.com>
2024-10-26 04:09:02 +0900

    [Silver IV] Title: 수열 정렬, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N], B = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N ; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = A[i];
+        }
+        Arrays.sort(B);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                if(A[i] == B[j]){
+                    sb.append(j).append(" ");
+                    B[j] = -1;
+                    break;
+                }
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
