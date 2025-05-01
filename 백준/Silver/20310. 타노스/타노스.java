71c17f47ac5ee612b5ab0859b809c0e1e46f7eae
taew0o <61934715+taew0o@users.noreply.github.com>
2025-05-01 16:59:15 +0900

    [Silver III] Title: 타노스, Time: 108 ms, Memory: 14380 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -5,8 +5,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        System.out.println("0".repeat(str.replaceAll("1", "").length() / 2)
-                + "1".repeat(str.replaceAll("0", "").length() / 2));
+        int remain0 = str.replaceAll("1", "").length() / 2;
+        int remain1 = str.replaceAll("0", "").length() / 2;
+
+        StringBuilder sb = new StringBuilder(str);
+        //1은 앞에서부터, 0은 뒤에서부터
+        int index = 0;
+        while (remain1 != 0){
+            if(sb.charAt(index) == '1'){
+                sb.deleteCharAt(index);
+                remain1--;
+                continue;
+            }
+            index++;
+        }
+
+        index = sb.length() - 1;
+        while (remain0 != 0){
+            if(sb.charAt(index) == '0'){
+                sb.deleteCharAt(index);
+                remain0--;
+            }
+            index--;
+        }
+
+        System.out.println(sb.toString());
     }
 }
 
