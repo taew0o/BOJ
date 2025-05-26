9f3b598a2b2da7c79571a3d378831af55a6f85eb
taew0o <61934715+taew0o@users.noreply.github.com>
2025-05-26 18:26:59 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 35.26 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static int calTime(int diff, int level, int time_cur, int time_prev){
+        if(diff <= level){
+            return time_cur;
+        }
+        return (diff - level) * (time_cur + time_prev) + time_cur;
+    }
+    public static long calAllTime(int[] diffs, int[] times, int level){
+        long sum = times[0];
+        for(int i = 1 ; i < diffs.length ; i++){
+            sum += calTime(diffs[i], level, times[i], times[i - 1]);
+        }
+        System.out.println(String.format("level : %d -> result : %d", level, sum));
+        return sum;
+    }
+    public static int binarySearch(int[] diffs, int[] times, long limit, int left, int right){
+        int result = Integer.MAX_VALUE;
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if(calAllTime(diffs, times, mid) <= limit){
+                result = Math.min(result, mid);
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    public static int solution(int[] diffs, int[] times, long limit) {
+        return binarySearch(diffs, times, limit, 1, 100_000);
+    }
+}
