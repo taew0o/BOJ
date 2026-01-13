54ceae9349321dc181790d32b6fecdc594c97df0
taew0o <61934715+taew0o@users.noreply.github.com>
2026-01-13 14:50:23 +0900

    [Gold III] Title: 세 용액, Time: 236 ms, Memory: 17100 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -2,54 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long[] arr;
-    static long minSum;
-    static int[] result;
-    public static long[] binarySearch(int left, int right){
-        long[] result = new long[]{-1, Long.MAX_VALUE}; //인덱스, 최소 합
-        int l = left + 1, r = right - 1;
-        while (l <= r){
-            int mid = (l + r) / 2;
-            long sum = arr[left] + arr[mid] + arr[right];
-            if(sum == 0) {
-                result[0] = mid; result[1] = 0;
-                break;
-            }else if (result[1] > Math.abs(sum)){
-                result[0] = mid;
-                result[1] = Math.abs(sum);
-            }
-            if(sum > 0){
-                r = mid - 1;
-            } else{
-                l = mid + 1;
-            }
-        }
-        return result;
-    }
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         Arrays.sort(arr);
-        minSum = Long.MAX_VALUE;
-        result = new int[3];
+
+        long minAbsSum = 3_000_000_000L;
+        int[] result = new int[3];
         for(int i = 0 ; i < N - 2 ; i++){
-            for(int j = N - 1 ; j > i + 1 ; j--){
-                long[] now = binarySearch(i, j);
-                int mid = (int) now[0];
-                long sum = now[1];
-                if(sum == 0){
-                    System.out.println(arr[i] + " " + arr[mid] + " " + arr[j]);
-                    return;
-                } else if(minSum > sum){
-                    minSum = sum;
-                    result[0] = i; result[1] = mid; result[2] = j;
+            int left = i + 1, right = N - 1;
+            while (left < right){
+                long sum = (long) arr[i] + arr[left] + arr[right];
+                if(Math.abs(sum) < minAbsSum){
+                    minAbsSum = Math.abs(sum);
+                    result[0] = arr[i]; result[1] = arr[left]; result[2] = arr[right];
+                }
+                if (sum == 0){
+                    break;
+                } else if(sum < 0){
+                    left++;
+                } else{
+                    right--;
                 }
             }
         }
-        System.out.println(arr[result[0]] + " " + arr[result[1]] + " " + arr[result[2]]);
+        System.out.println(result[0] + " " + result[1] + " " + result[2]);
     }
 }
