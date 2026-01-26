25144aabcc60ca1fae554b0f42b2304d5fc7fcdb
taew0o <61934715+taew0o@users.noreply.github.com>
2026-01-26 15:50:30 +0900

    [Gold I] Title: 계단 수, Time: 128 ms, Memory: 22900 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[][][] dp = new long[N + 1][10][1024];
+        long MOD = 1_000_000_000L;
+        for(int i = 1 ; i <= 9 ; i++){
+            dp[1][i][1 << i] = 1;
+        }
+        for(int len = 2 ; len <= N ; len++){
+            for(int i = 0 ; i <= 9 ; i++){
+                for(int mask = 0 ; mask < 1024 ; mask++){
+                    if(dp[len - 1][i][mask] == 0) continue;
+                    if(i > 0){
+                        int nextNumber = i - 1;
+                        int nextMask = mask | (1 << nextNumber);
+                        dp[len][nextNumber][nextMask] = (dp[len][nextNumber][nextMask] + dp[len - 1][i][mask]) % MOD;
+                    }
+                    if(i < 9){
+                        int nextNumber = i + 1;
+                        int nextMask = mask | (1 << nextNumber);
+                        dp[len][nextNumber][nextMask] = (dp[len][nextNumber][nextMask] + dp[len - 1][i][mask]) % MOD;
+                    }
+                }
+            }
+        }
+        long result = 0;
+        for(int i = 0 ; i <= 9 ; i++){
+            result = (result + dp[N][i][1023]) % MOD;
+        }
+        System.out.println(result);
+    }
+}
