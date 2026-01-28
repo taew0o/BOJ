c8613566813531244685cabc5f80fb8747cec256
taew0o <61934715+taew0o@users.noreply.github.com>
2026-01-28 14:59:24 +0900

    [Gold I] Title: 트리의 순회, Time: 388 ms, Memory: 41936 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -4,22 +4,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static int N, index;
     public static int[] inOrder, postOrder, preOrder;
+    public static int[] inOrderPos;
     public static void divideAndConquer(int inStart, int postStart , int len){
-        if(len == 0) return;
+        if(len <= 0) return;
         //post Order에서 root의 위치 : postStart + len - 1;
         int root = postOrder[postStart + len - 1];
         preOrder[index ++] = root;
         //길이 1이라면 탐색 종료
-        if(len == 1) return;
         //in order에서 root의 위치 찾기
-        int inRootIndex = -1;
-        for(int i = inStart ; i < inStart + len ; i++){
-            if(inOrder[i] == root) {
-                inRootIndex = i;
-                break;
-            }
-        }
-        int leftLen = inRootIndex - inStart, rightLen = (inStart + len - 1) - inRootIndex;
+        int inRootIndex = inOrderPos[root];
+        int leftLen = inRootIndex - inStart;
+        int rightLen = (inStart + len - 1) - inRootIndex;
         //LEFT 탐색
         divideAndConquer(inStart, postStart, leftLen);
         //RIGHT 탐색
@@ -31,9 +26,11 @@ public class Main {
         N =  Integer.parseInt(br.readLine());
         index = 0;
         inOrder = new int[N]; postOrder = new int[N]; preOrder = new int[N];
+        inOrderPos = new int[N + 1];
         StringTokenizer st1 = new StringTokenizer(br.readLine()), st2 = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i++){
             inOrder[i] = Integer.parseInt(st1.nextToken());
+            inOrderPos[inOrder[i]] = i;
             postOrder[i] = Integer.parseInt(st2.nextToken());
         }
         divideAndConquer(0, 0, N);
