602ef0ccf97dbed59d88d812b50a73f2ece63a7d
taew0o <61934715+taew0o@users.noreply.github.com>
2026-02-23 17:29:29 +0900

    [level 2] Title: 구명보트, Time: 12.00 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                left ++;
+            }
+            right--;
+            answer++;   
+        }
+        return answer;
+    }
+}
